cea6b7ce6843124f4b3968370ccb904e839dd828
AnMarMag569 <m.gordeeva63@gmail.com>
2024-12-03 21:00:53 +0100

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -1,17 +1,22 @@
 from pprint import pprint
 def introspection_info(obj):
+  info = {
+    'Тип объекта': type(obj).__name__,
+    'Список атрибутов': [],
+    'Список методов': [],
+    'Модуль, к которому принадлежит объект': obj.__module__ if hasattr(obj, '__module__') else 'Бесхозный обЪект',
+    'Документация': obj.__doc__ if obj.__doc__ else 'Документации нет (('
+  }
 
-  info = {}
-  info['Тип объекта:'] = type(obj)
-  info['Список атрибутов :'] = dir(obj)
-  info['Список методов:'] = [method for method in dir(obj) if callable(getattr(obj, method))]
-  info['Модуль, к которому принадлежит объект :'] = obj.__module__ if hasattr(obj, '__module__') else 'У объекта нет модуля'
-  info['Документация:'] = obj.__doc__ if obj.__doc__ else 'Документации нет (('
-
+  for attr in dir(obj):
+    if callable(getattr(obj, attr)):
+      info['Список методов'].append(attr)
+    else:
+      info['Список атрибутов'].append(attr)
   return info
 
 rezult = introspection_info([1,2,3])
 pprint(rezult)
 print()
-rezult = rezult = introspection_info(introspection_info)
+rezult = introspection_info(introspection_info)
 pprint(rezult)
